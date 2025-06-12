933d43ba62c0dbcd0723cfe19676298f8f7e2f64
Felicia <felicialewa19@gmail.com>
2025-06-12 15:55:13 +0700

    Initial Commit

---
+++ GlassesApp/GlassesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlassesAppApp.swift
+//  GlassesApp
+//
+//  Created by Felicia Stevany Lewa on 12/06/25.
+//
+
+import SwiftUI
+
+@main
+struct GlassesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GlassesAppTests/GlassesAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  GlassesAppTests.swift
+//  GlassesAppTests
+//
+//  Created by Felicia Stevany Lewa on 12/06/25.
+//
+
+import Testing
+@testable import GlassesApp
+
+struct GlassesAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
